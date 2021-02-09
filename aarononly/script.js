121c50753b89257586fb221c49a27f76f3807b7d
waarongoo <72639797+waarongoo@users.noreply.github.com>
2021-02-09 11:12:02 -0800

    Delete aarononly directory

--- aarononly/script.js
+++
@@ -1,7 +0,0 @@
-    function checkPswd() {
-      var confirmPassword = "103323";
-      var password = document.getElementById("pswd").value;
-      if (password == confirmPassword) {
-        window.location = "https://cdn.aww7.repl.co/adminserver";
-      }
-    }
